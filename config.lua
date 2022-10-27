78dd20775224266eea73aec9f35665b50f7c2f5c
w1ll44 <116809105+w1ll44@users.noreply.github.com>
2022-10-27 12:51:49 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,28 @@
+Config = {}
+
+Config.PedModel = "cs_lamardavis"
+
+Config.Payed = 108
+
+Config.VehicleSpawn = vector4(63.45, -1416.43, 27.61, 86.35)
+
+Config.FuelSystem = 'LegacyFuel'
+
+Config.Wanted = false
+
+Config.PedCoords = vector4(68.28, -1416.77, 28.5, 87.11)
+
+Config.Vehicle = "fireae86"
+
+Config.DoobieDeliverys = { -- Random delivery houses.
+    vector3(944.44, -463.19, 61.55),
+    vector3(970.42, -502.5, 62.14),
+    vector3(1099.5, -438.65, 67.79),
+    vector3(1229.6, -725.41, 60.96),
+    vector3(288.05, -1094.98, 29.42),
+    vector3(-32.35, -1446.46, 31.89),
+    vector3(1297.25, -1618.04, 54.58),
+    vector3(1381.98, -1544.75, 57.11),
+    vector3(1245.4, -1626.85, 53.28),
+    vector3(315.09, -128.31, 69.98),
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'w1ll#4444'
+description 'Doobie Delivery QBCore'
+
+shared_scripts {
+    'config.lua'
+}
+
+server_scripts {
+    'server/*.lua'
+}
+
+client_scripts {
+    'client/*.lua'
+}
+
+lua54 'yes'
